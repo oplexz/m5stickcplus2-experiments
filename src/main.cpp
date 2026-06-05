dc985d6f65e9d9205fb43123b5dec3c8b96ca6f3
Daniil Isakov <daniil.isakov@hotmail.com>
2026-06-06 01:49:14 +0300

    refactor: integrate screen management by registering various screen classes in main setup

--- src/main.cpp
+++ src/main.cpp
@@ -9,98 +9,73 @@
 #include <IrRepeatSender.h>
 #include <BleKeyboard.h>
 
+#include "screens/ScreenManager.h"
+#include "screens/ListScreen.h"
+#include "screens/IrMenuScreen.h"
+#include "screens/BrightnessScreen.h"
+#include "screens/IrBruteforceScreen.h"
+#include "screens/IrSendScreen.h"
+#include "screens/IrRepeatScreen.h"
+#include "screens/LampRemoteScreen.h"
+#include "screens/BleKeyboardScreen.h"
+
 static M5GFX screen;
 
-static constexpr uint8_t kButtonUpPin = 35;     // Up
-static constexpr uint8_t kButtonDownPin = 39;   // Down
-static constexpr uint8_t kButtonSelectPin = 37; // Select
+static constexpr uint8_t kButtonUpPin     = 35;
+static constexpr uint8_t kButtonDownPin   = 39;
+static constexpr uint8_t kButtonSelectPin = 37;
+static constexpr uint8_t kIrPin           = 19;
+
+static constexpr uint32_t kRepeatDelayMs    = 500;
+static constexpr uint32_t kRepeatIntervalMs = 33;
+static constexpr uint32_t kLongPressMs      = 600;
 
 static Button buttonUp(kButtonUpPin);
 static Button buttonDown(kButtonDownPin);
 static Button buttonSelect(kButtonSelectPin);
 
-static constexpr uint8_t kIrPin = 19; // M5StickC Plus2 IR LED
-
 static constexpr IrCommand kLampCommands[] = {
-    {"White/Yellow",  0x00, 0x18},
-    {"Yellow>White",  0x00, 0x30},
-    {"Colorful 1",    0x00, 0x38},
-    {"Colorful 2",    0x00, 0x4A},
-    {"Off",           0x00, 0x62},
+    {"White/Yellow", 0x00, 0x18},
+    {"Yellow>White", 0x00, 0x30},
+    {"Colorful 1",   0x00, 0x38},
+    {"Colorful 2",   0x00, 0x4A},
+    {"Off",          0x00, 0x62},
 };
-static constexpr size_t kLampCommandCount = sizeof(kLampCommands) / sizeof(kLampCommands[0]);
+static constexpr size_t kLampCommandCount =
+    sizeof(kLampCommands) / sizeof(kLampCommands[0]);
+
 static IrRemote lampRemote(screen, kIrPin, kLampCommands, kLampCommandCount);
 
-// Root menu
-static constexpr const char* kItems[] = {
-    "Brightness",
-    "IR Tools",
-    "BLE Keyboard",
-};
-static constexpr size_t kItemCount = sizeof(kItems) / sizeof(kItems[0]);
-static ScrollList list(screen, kItems, kItemCount);
+static constexpr const char* kMainItems[] = {"Brightness", "IR Tools", "BLE Keyboard"};
+static ScrollList mainList(screen, kMainItems, 3);
 
-static constexpr int kBrightnessItemIndex   = 0;
-static constexpr int kIrToolsItemIndex      = 1;
-static constexpr int kBleKeyboardItemIndex  = 2;
-
-// IR submenu
 static constexpr const char* kIrItems[] = {
-    "Lamp Remote",
-    "IR Bruteforce",
-    "IR Send",
-    "IR Repeat",
+    "Lamp Remote", "IR Bruteforce", "IR Send", "IR Repeat"
 };
-static constexpr size_t kIrItemCount = sizeof(kIrItems) / sizeof(kIrItems[0]);
-static ScrollList irList(screen, kIrItems, kIrItemCount);
+static ScrollList irList(screen, kIrItems, 4);
 
-static constexpr int kIrLampItemIndex      = 0;
-static constexpr int kIrBruteforceItemIndex = 1;
-static constexpr int kIrSendItemIndex      = 2;
-static constexpr int kIrRepeatItemIndex    = 3;
-
-static ValueEditor valueEditor(screen);
-
-static IrBruteforce irBruteforce(screen, kIrPin);
-static IrCodeSender irCodeSender(screen, kIrPin);
+static ValueEditor    valueEditor(screen);
+static IrBruteforce   irBruteforce(screen, kIrPin);
+static IrCodeSender   irCodeSender(screen, kIrPin);
 static IrRepeatSender irRepeatSender(screen, kIrPin);
-static BleKeyboard bleKeyboard(screen, "M5 Macro", 4321);
+static BleKeyboard    bleKeyboard(screen, "M5 Macro", 4321);
 
-static constexpr uint32_t kRepeatDelayMs   = 500;
-static constexpr uint32_t kRepeatIntervalMs = 33;
-static constexpr uint32_t kLongPressMs     = 600;
+static ListScreen         listScreen(mainList);
+static IrMenuScreen       irMenuScreen(irList, buttonSelect, kLongPressMs);
+static BrightnessScreen   brightnessScreen(screen, valueEditor,
+                                           buttonUp, buttonDown,
+                                           kRepeatDelayMs, kRepeatIntervalMs);
+static IrBruteforceScreen irBruteforceScreen(irBruteforce);
+static IrSendScreen       irSendScreen(irCodeSender,
+                                       buttonUp, buttonDown, buttonSelect,
+                                       kRepeatDelayMs, kRepeatIntervalMs, kLongPressMs);
+static IrRepeatScreen     irRepeatScreen(irRepeatSender,
+                                         buttonUp, buttonDown, buttonSelect,
+                                         kRepeatDelayMs, kRepeatIntervalMs, kLongPressMs);
+static LampRemoteScreen   lampRemoteScreen(lampRemote, buttonSelect, kLongPressMs);
+static BleKeyboardScreen  bleKeyboardScreen(bleKeyboard, buttonSelect, kLongPressMs);
 
-enum class ScreenMode
-{
-    List,
-    IrMenu,
-    Editor,
-    IrBruteforce,
-    IrSend,
-    IrRepeat,
-    LampRemote,
-    BleKeyboard,
-};
-
-static ScreenMode screenMode = ScreenMode::List;
-static uint32_t upPressStartMs = 0;
-static uint32_t upLastRepeatMs = 0;
-static uint32_t downPressStartMs = 0;
-static uint32_t downLastRepeatMs = 0;
-static uint32_t selectPressStartMs = 0;
-
-static uint8_t percentToBrightness(int percent)
-{
-    if (percent < 0)
-    {
-        percent = 0;
-    }
-    if (percent > 100)
-    {
-        percent = 100;
-    }
-    return static_cast<uint8_t>((percent * 255) / 100);
-}
+static ScreenManager manager;
 
 void setup()
 {
@@ -122,439 +97,22 @@ void setup()
     valueEditor.setStep(5);
     valueEditor.setValue(50);
 
-    list.draw();
+    manager.registerScreen(ScreenId::List,         &listScreen);
+    manager.registerScreen(ScreenId::IrMenu,       &irMenuScreen);
+    manager.registerScreen(ScreenId::Brightness,   &brightnessScreen);
+    manager.registerScreen(ScreenId::IrBruteforce, &irBruteforceScreen);
+    manager.registerScreen(ScreenId::IrSend,       &irSendScreen);
+    manager.registerScreen(ScreenId::IrRepeat,     &irRepeatScreen);
+    manager.registerScreen(ScreenId::LampRemote,   &lampRemoteScreen);
+    manager.registerScreen(ScreenId::BleKeyboard,  &bleKeyboardScreen);
+
+    manager.begin(ScreenId::List);
 }
 
 void loop()
 {
-    bool updated = false;
-
-    if (screenMode == ScreenMode::List)
-    {
-        buttonUp.read();
-        buttonDown.read();
-        buttonSelect.read();
-
-        if (buttonUp.pressed())
-        {
-            updated = list.moveUp(true);
-        }
-
-        if (buttonDown.pressed())
-        {
-            updated = list.moveDown(true) || updated;
-        }
-
-        if (buttonSelect.pressed())
-        {
-            if (list.selectedIndex() == kBrightnessItemIndex)
-            {
-                screenMode = ScreenMode::Editor;
-                valueEditor.draw();
-            }
-            else if (list.selectedIndex() == kIrToolsItemIndex)
-            {
-                screenMode = ScreenMode::IrMenu;
-                selectPressStartMs = 0;
-                irList.draw();
-            }
-            else if (list.selectedIndex() == kBleKeyboardItemIndex)
-            {
-                screenMode = ScreenMode::BleKeyboard;
-                selectPressStartMs = 0;
-                bleKeyboard.start();
-            }
-        }
-
-        if (updated)
-        {
-            list.draw();
-        }
-    }
-    else if (screenMode == ScreenMode::IrMenu)
-    {
-        buttonUp.read();
-        buttonDown.read();
-
-        if (buttonUp.pressed())
-        {
-            updated = irList.moveUp(true);
-        }
-
-        if (buttonDown.pressed())
-        {
-            updated = irList.moveDown(true) || updated;
-        }
-
-        uint32_t now = millis();
-        bool selectState = (buttonSelect.read() == Button::PRESSED);
-        bool selectChanged = buttonSelect.has_changed();
-
-        if (selectState && selectChanged)
-        {
-            selectPressStartMs = now;
-        }
-        else if (!selectState && selectChanged && selectPressStartMs != 0)
-        {
-            if (now - selectPressStartMs < kLongPressMs)
-            {
-                selectPressStartMs = 0;
-                if (irList.selectedIndex() == kIrLampItemIndex)
-                {
-                    screenMode = ScreenMode::LampRemote;
-                    lampRemote.draw();
-                }
-                else if (irList.selectedIndex() == kIrBruteforceItemIndex)
-                {
-                    screenMode = ScreenMode::IrBruteforce;
-                    irBruteforce.start();
-                }
-                else if (irList.selectedIndex() == kIrSendItemIndex)
-                {
-                    screenMode = ScreenMode::IrSend;
-                    irCodeSender.draw();
-                }
-                else if (irList.selectedIndex() == kIrRepeatItemIndex)
-                {
-                    screenMode = ScreenMode::IrRepeat;
-                    irRepeatSender.draw();
-                }
-            }
-            else
-            {
-                selectPressStartMs = 0;
-            }
-        }
-        else if (selectState && selectPressStartMs != 0 &&
-                 now - selectPressStartMs >= kLongPressMs)
-        {
-            selectPressStartMs = 0;
-            screenMode = ScreenMode::List;
-            list.draw();
-        }
-
-        if (updated)
-        {
-            irList.draw();
-        }
-    }
-    else if (screenMode == ScreenMode::Editor)
-    {
-        uint32_t now = millis();
-        bool upState = (buttonUp.read() == Button::PRESSED);
-        bool upChanged = buttonUp.has_changed();
-
-        if (!upState)
-        {
-            upPressStartMs = 0;
-            upLastRepeatMs = 0;
-        }
-        else if (upChanged)
-        {
-            if (valueEditor.decrease())
-            {
-                screen.setBrightness(percentToBrightness(valueEditor.value()));
-                valueEditor.draw();
-            }
-            upPressStartMs = now;
-            upLastRepeatMs = now;
-        }
-        else if (upPressStartMs != 0 &&
-                 now - upPressStartMs >= kRepeatDelayMs &&
-                 now - upLastRepeatMs >= kRepeatIntervalMs)
-        {
-            if (valueEditor.decrease())
-            {
-                screen.setBrightness(percentToBrightness(valueEditor.value()));
-                valueEditor.draw();
-            }
-            upLastRepeatMs = now;
-        }
-
-        bool downState = (buttonDown.read() == Button::PRESSED);
-        bool downChanged = buttonDown.has_changed();
-
-        if (!downState)
-        {
-            downPressStartMs = 0;
-            downLastRepeatMs = 0;
-        }
-        else if (downChanged)
-        {
-            if (valueEditor.increase())
-            {
-                screen.setBrightness(percentToBrightness(valueEditor.value()));
-                valueEditor.draw();
-            }
-            downPressStartMs = now;
-            downLastRepeatMs = now;
-        }
-        else if (downPressStartMs != 0 &&
-                 now - downPressStartMs >= kRepeatDelayMs &&
-                 now - downLastRepeatMs >= kRepeatIntervalMs)
-        {
-            if (valueEditor.increase())
-            {
-                screen.setBrightness(percentToBrightness(valueEditor.value()));
-                valueEditor.draw();
-            }
-            downLastRepeatMs = now;
-        }
-
-        if (buttonSelect.pressed())
-        {
-            screenMode = ScreenMode::List;
-            list.draw();
-        }
-    }
-    else if (screenMode == ScreenMode::IrBruteforce)
-    {
-        buttonUp.read();
-        buttonDown.read();
-
-        irBruteforce.tick();
-
-        if (buttonSelect.pressed())
-        {
-            irBruteforce.stop();
-            screenMode = ScreenMode::IrMenu;
-            irList.draw();
-        }
-    }
-    else if (screenMode == ScreenMode::IrSend)
-    {
-        uint32_t now = millis();
-
-        bool upState = (buttonUp.read() == Button::PRESSED);
-        bool upChanged = buttonUp.has_changed();
-
-        if (!upState)
-        {
-            upPressStartMs = 0;
-            upLastRepeatMs = 0;
-        }
-        else if (upChanged)
-        {
-            irCodeSender.prev();
-            upPressStartMs = now;
-            upLastRepeatMs = now;
-        }
-        else if (upPressStartMs != 0 &&
-                 now - upPressStartMs >= kRepeatDelayMs &&
-                 now - upLastRepeatMs >= kRepeatIntervalMs)
-        {
-            irCodeSender.prev();
-            upLastRepeatMs = now;
-        }
-
-        bool downState = (buttonDown.read() == Button::PRESSED);
-        bool downChanged = buttonDown.has_changed();
-
-        if (!downState)
-        {
-            downPressStartMs = 0;
-            downLastRepeatMs = 0;
-        }
-        else if (downChanged)
-        {
-            irCodeSender.next();
-            downPressStartMs = now;
-            downLastRepeatMs = now;
-        }
-        else if (downPressStartMs != 0 &&
-                 now - downPressStartMs >= kRepeatDelayMs &&
-                 now - downLastRepeatMs >= kRepeatIntervalMs)
-        {
-            irCodeSender.next();
-            downLastRepeatMs = now;
-        }
-
-        // Select: short press = send, long press = back to IR menu
-        bool selectState = (buttonSelect.read() == Button::PRESSED);
-        bool selectChanged = buttonSelect.has_changed();
-
-        if (selectState && selectChanged)
-        {
-            selectPressStartMs = now;
-        }
-        else if (!selectState && selectChanged && selectPressStartMs != 0)
-        {
-            if (now - selectPressStartMs < kLongPressMs)
-            {
-                irCodeSender.send();
-            }
-            selectPressStartMs = 0;
-        }
-        else if (selectState && selectPressStartMs != 0 &&
-                 now - selectPressStartMs >= kLongPressMs)
-        {
-            selectPressStartMs = 0;
-            screenMode = ScreenMode::IrMenu;
-            irList.draw();
-        }
-    }
-    else if (screenMode == ScreenMode::IrRepeat)
-    {
-        uint32_t now = millis();
-
-        bool upState = (buttonUp.read() == Button::PRESSED);
-        bool upChanged = buttonUp.has_changed();
-
-        if (!upState)
-        {
-            upPressStartMs = 0;
-            upLastRepeatMs = 0;
-        }
-        else if (upChanged)
-        {
-            irRepeatSender.prev();
-            upPressStartMs = now;
-            upLastRepeatMs = now;
-        }
-        else if (upPressStartMs != 0 &&
-                 now - upPressStartMs >= kRepeatDelayMs &&
-                 now - upLastRepeatMs >= kRepeatIntervalMs)
-        {
-            irRepeatSender.prev();
-            upLastRepeatMs = now;
-        }
-
-        bool downState = (buttonDown.read() == Button::PRESSED);
-        bool downChanged = buttonDown.has_changed();
-
-        if (!downState)
-        {
-            downPressStartMs = 0;
-            downLastRepeatMs = 0;
-        }
-        else if (downChanged)
-        {
-            irRepeatSender.next();
-            downPressStartMs = now;
-            downLastRepeatMs = now;
-        }
-        else if (downPressStartMs != 0 &&
-                 now - downPressStartMs >= kRepeatDelayMs &&
-                 now - downLastRepeatMs >= kRepeatIntervalMs)
-        {
-            irRepeatSender.next();
-            downLastRepeatMs = now;
-        }
-
-        irRepeatSender.tick();
-
-        // Select: short press = toggle sending, long press = back to IR menu
-        bool selectState = (buttonSelect.read() == Button::PRESSED);
-        bool selectChanged = buttonSelect.has_changed();
-
-        if (selectState && selectChanged)
-        {
-            selectPressStartMs = now;
-        }
-        else if (!selectState && selectChanged && selectPressStartMs != 0)
-        {
-            if (now - selectPressStartMs < kLongPressMs)
-            {
-                if (irRepeatSender.isSending())
-                {
-                    irRepeatSender.stopSending();
-                }
-                else
-                {
-                    irRepeatSender.startSending();
-                }
-            }
-            selectPressStartMs = 0;
-        }
-        else if (selectState && selectPressStartMs != 0 &&
-                 now - selectPressStartMs >= kLongPressMs)
-        {
-            irRepeatSender.stopSending();
-            selectPressStartMs = 0;
-            screenMode = ScreenMode::IrMenu;
-            irList.draw();
-        }
-    }
-    else if (screenMode == ScreenMode::LampRemote)
-    {
-        uint32_t now = millis();
-
-        buttonUp.read();
-        bool upChanged = buttonUp.has_changed();
-        if (buttonUp.read() == Button::PRESSED && upChanged)
-        {
-            if (lampRemote.moveUp(true))
-            {
-                lampRemote.draw();
-            }
-        }
-
-        buttonDown.read();
-        bool downChanged = buttonDown.has_changed();
-        if (buttonDown.read() == Button::PRESSED && downChanged)
-        {
-            if (lampRemote.moveDown(true))
-            {
-                lampRemote.draw();
-            }
-        }
-
-        // Select: short press = send, long press = back to IR menu
-        bool selectState = (buttonSelect.read() == Button::PRESSED);
-        bool selectChanged = buttonSelect.has_changed();
-
-        if (selectState && selectChanged)
-        {
-            selectPressStartMs = now;
-        }
-        else if (!selectState && selectChanged && selectPressStartMs != 0)
-        {
-            if (now - selectPressStartMs < kLongPressMs)
-            {
-                lampRemote.sendSelected();
-            }
-            selectPressStartMs = 0;
-        }
-        else if (selectState && selectPressStartMs != 0 &&
-                 now - selectPressStartMs >= kLongPressMs)
-        {
-            selectPressStartMs = 0;
-            screenMode = ScreenMode::IrMenu;
-            irList.draw();
-        }
-    }
-    else if (screenMode == ScreenMode::BleKeyboard)
-    {
-        uint32_t now = millis();
-        bleKeyboard.tick();
-
-        buttonUp.read();
-        buttonDown.read();
-
-        bool selectState = (buttonSelect.read() == Button::PRESSED);
-        bool selectChanged = buttonSelect.has_changed();
-
-        if (selectState && selectChanged)
-        {
-            selectPressStartMs = now;
-        }
-        else if (!selectState && selectChanged && selectPressStartMs != 0)
-        {
-            if (now - selectPressStartMs < kLongPressMs)
-            {
-                bleKeyboard.runMacro();
-            }
-            selectPressStartMs = 0;
-        }
-        else if (selectState && selectPressStartMs != 0 &&
-                 now - selectPressStartMs >= kLongPressMs)
-        {
-            bleKeyboard.stop();
-            selectPressStartMs = 0;
-            screenMode = ScreenMode::List;
-            list.draw();
-        }
-    }
-
+    uint32_t now = millis();
+    Inputs inputs{buttonUp, buttonDown, buttonSelect, now};
+    manager.tick(inputs);
     delay(10);
 }
